3720af6ed965a6afc8d00ae64d50ef7aeceabbae
mesaaransh <mesaaransh@gmail.com>
2025-03-25 00:39:00 +0530

    images issue fixed

--- src/Projects/projectlist.js
+++ src/Projects/projectlist.js
@@ -4,7 +4,7 @@ var projectlist = [
         name: "BoxBox",
         description: "An F1 race strategy optimizer leveraging telemetry data, tyre degradation modeling, and pit stop simulations to predict the fastest race plan.",
         'imgURL': [
-            'src/Projects/assets/boxbox/1.jpg',
+            '/assets/boxbox/1.jpg',
         ]
 
     },
@@ -12,8 +12,8 @@ var projectlist = [
         name: "Splitzee",
         description: "Splitzee is a smart bill-splitting app that enables seamless group payments with real-time updates and secure transactions.",
         'imgURL': [
-            'src/Projects/assets/splitzee/1.jpg',
-            'src/Projects/assets/splitzee/2.jpg',
+            '/assets/splitzee/1.jpg',
+            '/assets/splitzee/2.jpg',
         ]
         
 
@@ -22,8 +22,8 @@ var projectlist = [
         name: "EasyFind",
         description: "EasyFind is an AI-powered lost and found platform that helps users quickly locate and recover misplaced items.",
         'imgURL': [
-            'src/Projects/assets/easyfind/1.jpg',
-            'src/Projects/assets/easyfind/2.jpg',
+            '/assets/easyfind/1.jpg',
+            '/assets/easyfind/2.jpg',
         ]
 
     },
@@ -31,7 +31,7 @@ var projectlist = [
         name: "Raptus",
         description: "Raptus is an interactive online game featuring a dynamic 3D map and real-time multiplayer experiences.",
         'imgURL': [
-            'src/Projects/assets/raptus/1.jpg',
+            '/assets/raptus/1.jpg',
         ]
 
     },
