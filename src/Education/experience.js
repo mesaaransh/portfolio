f387f8e52504fc58b485d1d48f13e3aab2acc463
mesaaransh <mesaaransh@gmail.com>
2025-07-24 22:21:26 +0530

    experience fixed

--- src/Education/experience.js
+++ src/Education/experience.js
@@ -1,5 +1,12 @@
 let experience = [
     {
+        title: 'Data Science Intern',
+        company: 'SlashCode Softwares Pvt. Ltd.',
+        startDate: 'Jun 25',
+        endDate: 'Jul 25',
+        location: 'Patiala, India'
+    },
+    {
         title: 'Full Stack Intern',
         company: 'ACT Center, School of Liberal Arts & Sciences',
         startDate: 'Apr 24',
@@ -9,15 +16,15 @@ let experience = [
     {
         title: 'Full Stack Intern',
         company: 'PraxisForge Ltd.',
-        startDate: 'Aug 24',
-        endDate: 'Dec 24',
+        startDate: 'Jun 24',
+        endDate: 'Jul 24',
         location: 'Remote | Delhi, India'
     },
     {
         title: 'Research Intern',
-        company: 'ACT Center, School of Liberal Arts & Sciences',
-        startDate: 'Sep 22',
-        endDate: 'Sep 23',
+        company: 'Pollution Control Board, Punjab',
+        startDate: 'Jun 23',
+        endDate: 'Jul 23',
         location: 'Chandigarh, India'
     }
 ]
